c2736d6b215a7e1a358da6d0b97a0d33bc2ae956
Babanenkov Sergii <graydisel@gmail.com>
2026-08-15 20:31:18 +0300

    CI/CD Pipeline

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    console.log('Backend is running! Docker Hot Reloadasdasdasdasdd!');
+    console.log('Backend is running!');
     return 'Freelance CRM Backend is running!';
   }
 }
